c8833591ebc87682811465eb6b8320dd1b2c832f
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2024-12-22 01:39:52 -0800

    develop: all doctors section

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     const [token, setToken] = useState(true);
     return (
         <div className='flex items-center justify-between text-sm py-4 mb-5 border-b border-b-gray-400'>
-            <div>
+            <div onClick={()=>navigate('/')}>
                 <img className='w-44 cursor-pointer' src={assets.logo} alt="logo" />
             </div>
             <ul className='hidden md:flex items-center gap-5 font-medium'>
