f78db89f730b20f7a6cd401f6a9880a4613ad342
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2024-12-24 13:35:38 -0800

    add mongoose explanation commnet

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
+    //reacts connection
     mongoose.connection.on('connected', ()=>console.log('DB connected'));
+    //emits connection
     await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`)
 }
 
