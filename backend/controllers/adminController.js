a8f53f4b27de2e616352d1c5188f6d5105de1735
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2024-12-24 20:24:48 -0800

    develop: backend server testing with router + middleware setup

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,17 @@
+
+
+
+//API for adding doctor
+const addDoctor = async (req,res) => {
+    try {
+        const {name,email,password,speciality,degree,experience,about,available,fees,address,date,slots_booked} = req.body
+        const imageFile = req.file
+
+        console.log({name,email,password,speciality,degree,experience,about,available,fees,address,date,slots_booked},imageFile);
+        
+    } catch (error) {
+        
+    }
+}
+
+export {addDoctor}
