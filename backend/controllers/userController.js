f91354026e666fd8af3915249f780a9c5834443c
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2024-12-30 00:04:19 -0800

    develop: user acc register

---
+++ backend/controllers/userController.js
@@ -0,0 +1,41 @@
+import validator from "validator"
+import userModel from '../models/userModel.js'
+import bcrypt from "bcrypt"
+import jwt from 'jsonwebtoken'
+const registerUser = async (req,res) => {
+    const {name, email, password} = req.body
+    try {  
+        //validating all detail info
+        if (!name || !email || !password) {
+            return res.json({success:false,message:"Detail Missing!"})
+        }
+        //validating email
+        if(!validator.isEmail(email)){
+            return res.json({success:false,message:"Enter a valid email!"})
+        }
+        //validating password
+        if(password.length < 8) {
+            return res.json({success:false,message:"Enter a stronng password!"})
+        }
+        //hashing password
+        const salt = await bcrypt.genSalt(10)
+        const hashpassword = await bcrypt.hash(password,salt)
+        const userData = {
+            name,
+            email,
+            password : hashpassword
+        }
+
+        const newUser = new userModel(userData)
+        const user = await newUser.save()
+        
+        const token = jwt.sign({id:user._id}, process.env.JWT_SECRET)
+        res.json({success:true,token})
+
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:error.message})
+    }
+}
+
+export {registerUser}
--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
 import adminRouter from './routes/adminRoute.js'
 import docRouter from './routes/doctorRoute.js'
+import userRouter from './routes/userRoute.js'
 //app config
 const app = express()
 const port = process.env.PORT || 4000
@@ -20,7 +21,8 @@ app.use('/api/admin',adminRouter)
 //localhost:4000/api/admin/add-doctor or /login or /all-doctors or /change-availablity
 app.use('/api/doctor',docRouter)
 //localhost:4000/api/doctor/getDocList
-
+app.use('/api/user/',userRouter)
+//localhost:4000/api/doctor/getDocList/register
 app.get('/',(req,res)=>{
     res.send('API WORKING')
 })
