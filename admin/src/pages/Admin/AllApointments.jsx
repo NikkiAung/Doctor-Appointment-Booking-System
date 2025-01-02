8c87ce3c0f780f0585225f82bcc8ea02f9335833
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-01-02 00:54:24 -0800

    develop: backend API for cancelling appointment from admin panel

--- admin/src/pages/Admin/AllApointments.jsx
+++ admin/src/pages/Admin/AllApointments.jsx
@@ -6,7 +6,7 @@ import { AppContext } from '../../context/AppContext'
 import { assets } from "../../assets/assets_admin/assets"
 
 const AllApointments = () => {
-  const {aToken,appointments,getAllAppointments} = useContext(AdminContext)
+  const {aToken,appointments,getAllAppointments,cancelAppointment} = useContext(AdminContext)
   const {calculateAge,slotDateFormat,currency} = useContext(AppContext)
   useEffect(()=>{
     if(aToken){
@@ -41,7 +41,7 @@ const AllApointments = () => {
             {item.cancelled ? 
             <p className="text-red-400 text-xs font-medium">Cancelled</p>
             :
-            <img className='w-10 cursor-pointer' src={assets.cancel_icon} alt="" />
+            <img onClick={()=>cancelAppointment(item._id)} className='w-10 cursor-pointer' src={assets.cancel_icon} alt="" />
             }
           </div>
         ))}
