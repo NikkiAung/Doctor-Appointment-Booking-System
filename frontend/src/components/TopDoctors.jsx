e5751acc3fd55715def643715931437d976d8a0d
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-01-04 20:28:45 -0800

    develop: dynamic ui for doc availability

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -15,11 +15,26 @@ const TopDoctors = () => {
                     <div onClick={()=>navigate(`/appointment/${item._id}`)} className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500" key={index}>
                         <img className='bg-blue-50' src={item.image} alt="" />
                         <div className='p-4'>
-                            <div className='flex items-center gap-2 text-sm text-center text-green-500'>
-                                <span className='relative w-2 h-2 rounded-full bg-emerald-400'> 
-                                    <span className='absolute rounded-full inset-0 animate-ping bg-emerald-400'></span>
-                                </span>
-                                <p>Available</p>
+                            <div className={`flex items-center gap-2 text-sm text-center ${ item.available ? 'text-green-500' : 'text-gray-500'} `}>
+                                {
+                                    item.available
+                                    ? 
+                                    ( 
+                                        <>
+                                            <span className='relative w-2 h-2 rounded-full bg-emerald-400'> 
+                                                <span className='absolute rounded-full inset-0 animate-ping bg-emerald-400'></span>
+                                            </span>
+                                            <p>Available</p>
+                                        </>
+                                    )
+                                    :
+                                    (
+                                        <>  
+                                            <span className='w-2 h-2 rounded-full bg-gray-500'></span>
+                                            <p className='text-gray-500'>Not Available</p>
+                                        </>
+                                    )
+                                }
                             </div>
                             <p className='text-gray-900 text-lg font-medium'>{item.name}</p>
                             <p className='text-gray-600 text-sm'>{item.speciality}</p>
